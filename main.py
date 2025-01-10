23553928ac821e1d135f2ee8f61f4db5c00bd19d
Aarush Dixit <aarushdixit73androi@gmail.com>
2025-01-10 11:19:31 -0800

    Fixed some issues

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from src.project.pipeline.training_pipeline import training_pipeline
-from src.project.pipeline.deployment_pipeline import deployment_pipeline
 from src.project.utils.common import read_yaml
 
+import os
+
+os.environ['MLFLOW_TRACKING_URL']="https://dagshub.com/aarushdixit73androi/milk-quality-project.mlflow"
+os.environ['MLFLOW_TRACKING_USERNAME']="aarushdixit73androi"
+os.environ['MLFLOW_TRACKING_PASSWORD']="28b53480818ced457dfa2539ff696cb9d98be199"
+
+
 if __name__ == "__main__":
-    if input("Training Pipeline? (y/n)").lower() == "y":
-        config = read_yaml("config/config.yaml")
-        training_pipeline(config.data_ingestion.source)
-    else:
-        deployment_pipeline()
+    config = read_yaml("config/config.yaml")
+    training_pipeline(config.data_ingestion.source)
